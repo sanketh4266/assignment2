ee27fb11a1dada70e5541a0bcde8de8ec07dd45a
sanketh4266 <vukkalkarsaisanketh@gmail.com>
2023-02-07 20:56:37 +0530

    22

---
+++ StartsWithExample.java
@@ -0,0 +1,11 @@
+public class StartsWithExample {
+    // main method
+    public static void main(String args[])
+    {
+// input string
+        String s1="java string split method by javatpoint";
+        System.out.println(s1.startsWith("ja"));  // true
+        System.out.println(s1.startsWith("java string"));   // true
+        System.out.println(s1.startsWith("Java string"));  // false as 'j' and 'J' are different
+    }
+}
