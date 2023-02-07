1077770817926a6fce6e48d58d69fbf2b38d6536
sanketh4266 <vukkalkarsaisanketh@gmail.com>
2023-02-07 20:34:20 +0530

    7

--- Equals.java
+++ Equals.java
@@ -1,7 +1,10 @@
-public class Equals {
-    public static void main(String[] args){
-        String s1="java by javatpoint";
-        System.out.println(s1.endsWith("t"));
-        System.out.println(s1.endsWith("point"));
+public class Equals{
+        public static void main(String[] args){
+            String s1="javatpoint";
+            String s2="javatpoint";
+            String s3="JAVATPOINT";
+            String s4="python";
+            System.out.println(s1.equals(s2));
+            System.out.println(s1.equals(s3));
+            System.out.println(s1.equals(s4));
 }}
-
